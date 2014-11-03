bc968e08ec7a5f827ed8bdda7afabb99c48fc444
root <root@graphite1.graphite1>
2014-11-03 15:27:50 +0000

    adding monitoring stack checks

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,8 @@ class monit::params {
   case $::lsbdistcodename {
     'squeeze': {
     }
+    'trusty': {
+    }
     default: {
       fail("Module ${module_name} does not support ${::lsbdistcodename}")
     }

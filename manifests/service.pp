6b250bc5b78ccd5eb7f33ef77511ba5dcb7d2f18
Pierig Le Saux <lesaux@pythian.com>
2014-12-12 10:42:22 -0500

    new monit features RedHat and custom service support

--- manifests/service.pp
+++ manifests/service.pp
@@ -28,3 +28,35 @@ case $::osfamily {
 }
 
 }
+
+
+define monit::service::template( $process_name, $pid_file, $start_program, $stop_program, $port ) {
+
+case $::osfamily {
+
+  'debian': {
+    file { "/etc/monit/conf.d/${name}":
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template("monit/common/etc/monit/conf.d/template"),
+      notify  => Service['monit'],
+      require => [ File['conf.d'],Package['monit'] ],
+    }
+  }
+
+  'redhat': {
+    file { "/etc/monit.d/${name}":
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template("monit/common/etc/monit/conf.d/template"),
+      notify  => Service['monit'],
+      require => [ File['conf.d'],Package['monit'] ],
+    }
+
+  }
+
+}
+
+}
